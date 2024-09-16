1c5b735d0c6a205d582fe0d54fe7c4e0571884a4
Daniel Prado <dprado2024@alu.uct.cl>
2024-09-16 13:05:23 -0600

    Se termino la pagina principal, asimismo se hizo el js para generar ofertas destacadas automáticamente. se agrego el símbolo de notificación, mis postulaciones y la barra de navegación para las ofertas.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.getElementById("postulaciones-btn").addEventListener("click", function() {
+    window.location.href = "postulaciones.html"; // Redirecciona a la página de postulaciones
+});
+
+document.getElementById("notificaciones-btn").addEventListener("click", function() {
+    window.location.href = "notificaciones.html"; // Redirecciona a la página de notificaoines
+});
+document.getElementById("logo-user").addEventListener("click", function() {
+    window.location.href = "miperfil.html"; // Redirecciona a la página mi perfil
+});
+
+document.getElementById("cv-btn").addEventListener("click", function() {
+    window.location.href = "cv.html"; // Redirecciona a la página de postulaciones
+});
+
+document.getElementById("perfil-btn").addEventListener("click", function() {
+    window.location.href = "perfil.html"; // Redirecciona a la página de postulaciones
+});
+document.getElementById("cerrar-btn").addEventListener("click", function() {
+    window.location.href = "cerrrar.html"; // Redirecciona a la página de postulaciones
+});
