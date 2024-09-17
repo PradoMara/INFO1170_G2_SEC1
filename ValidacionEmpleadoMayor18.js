e0bb5b3a5b745e393c8c91e589c7f4dcfc57a4ee
Martinez-Nicolas <nmartinez2024@alu.uct.cl>
2024-09-17 17:52:41 -0300

    Pequeños cambios
    Cambios en el html de la politica y de los terminos, tambien cambios en los estilos.

--- ValidacionEmpleadoMayor18.js
+++ ValidacionEmpleadoMayor18.js
@@ -1,9 +1,8 @@
-// Función para mostrar/ocultar la sección de ayuda
 function toggleHelp() {
     var helpSection = document.getElementById("help-section");
     if (helpSection.style.display === "none" || helpSection.style.display === "") {
-        helpSection.style.display = "block"; // Mostrar la sección de ayuda
+        helpSection.style.display = "block"; 
     } else {
-        helpSection.style.display = "none"; // Ocultar la sección de ayuda
+        helpSection.style.display = "none"; 
     }
 }
