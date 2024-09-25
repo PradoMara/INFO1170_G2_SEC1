cfd609362176cb38a212ca85c250d11e27fc9e87
Martinez-Nicolas <nmartinez2024@alu.uct.cl>
2024-09-25 14:48:10 -0300

    Cambio pequeño
    Se agregaron tablas a la base de datos

--- basededatos.py
+++ basededatos.py
@@ -1,121 +1,146 @@
 import sqlite3
 
-conn = sqlite3.connect('mi_base_de_datos.db')
+conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Region (
-    id_Region INTEGER PRIMARY KEY,
-    nom_Region TEXT
+CREATE TABLE Region (
+    id_Region INT PRIMARY KEY,
+    nom_Region VARCHAR(20)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Ciudades (
-    Id_Ciudad INTEGER PRIMARY KEY,
-    Nom_Ciudad TEXT,
-    id_Region INTEGER,
+CREATE TABLE Ciudades (
+    Id_Ciudad INT PRIMARY KEY,
+    Nom_Ciudad VARCHAR(50),
+    id_Region INT,
     FOREIGN KEY (id_Region) REFERENCES Region(id_Region)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Usuarios (
-    id_Usuario INTEGER PRIMARY KEY,
-    nombre TEXT,
-    correo TEXT,
-    contraseña INTEGER,
-    direccion TEXT,
-    telefono TEXT,
-    id_Region INTEGER,
-    id_Ciudad INTEGER,
+CREATE TABLE Usuarios (
+    id_Usuario INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    correo VARCHAR(100),
+    contraseña INT,
+    direccion VARCHAR(100),
+    telefono VARCHAR(12),
+    id_Region INT,
+    id_Ciudad INT,
     FOREIGN KEY (id_Ciudad) REFERENCES Ciudades(Id_Ciudad),
     FOREIGN KEY (id_Region) REFERENCES Region(id_Region)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Soporte (
-    id_Soporte INTEGER PRIMARY KEY,
-    id_Usuario INTEGER,
-    id_Empresa INTEGER,
+CREATE TABLE Soporte (
+    id_Soporte INT PRIMARY KEY,
+    id_Usuario INT,
+    id_Empresa INT,
     FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Termino_condiciones (
-    id_Terminos INTEGER PRIMARY KEY,
+CREATE TABLE "Termino_condiciones" (
+    id_Terminos INT PRIMARY KEY,
     fecha_Actualizacion DATE,
-    contenido_privacidad TEXT,
-    version TEXT,
-    id_Empresa INTEGER
+    contenido_privacidad VARCHAR(100),
+    version VARCHAR(100),
+    id_Empresa INT
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Empresa (
-    id_Empresa INTEGER PRIMARY KEY,
-    nombre_Empresa TEXT,
-    direccion TEXT,
-    rut_Empresa TEXT,
-    des_Empresa TEXT,
-    id_Usuario INTEGER,
+CREATE TABLE Empresa (
+    id_Empresa INT PRIMARY KEY,
+    nombre_Empresa VARCHAR(100),
+    direccion VARCHAR(100),
+    rut_Empresa VARCHAR(50),
+    des_Empresa VARCHAR(500),
+    id_Usuario INT,
     FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Politica_Privacidad (
-    id_Privacidad INTEGER PRIMARY KEY,
+CREATE TABLE Politica_Privacidad (
+    id_Privacidad INT PRIMARY KEY,
     fecha_Actualizacion DATE,
-    contenido_privacidad TEXT,
-    version TEXT,
-    id_Empresa INTEGER
+    contenido_privacidad VARCHAR(100),
+    version VARCHAR(100),
+    id_Empresa INT
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Postulante (
-    id_Postulante INTEGER PRIMARY KEY,
-    nombre TEXT,
-    apellido TEXT,
-    edad INTEGER,
-    especialidad TEXT,
-    Experiencia TEXT,
-    curriculum TEXT,
-    id_Usuario INTEGER,
+CREATE TABLE Oferta_de_trabajo (
+    id_Oferta INT PRIMARY KEY,
+    descripcion VARCHAR(100),
+    requisitos VARCHAR(100),
+    ubicacion VARCHAR(100),
+    salario INT,
+    id_Empresa INT,
+    FOREIGN KEY (id_Empresa) REFERENCES Empresa(id_Empresa)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE Estadisticas (
+    id_Estadistica INT PRIMARY KEY,
+    is_Oferta INT,
+    visitas INT,
+    postulaciones INT,
+    contratos INT,
+    fecha_actualizacion DATETIME,
+    id_Oferta INT,
+    FOREIGN KEY (id_Oferta) REFERENCES Oferta_de_trabajo(id_Oferta)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE Postulante (
+    id_Postulante INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    edad INT,
+    especialidad VARCHAR(100),
+    Experiencia VARCHAR(100),
+    curriculum VARCHAR(100),
+    id_Usuario INT,
     FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Registro_de_actividades (
-    id_Actividad INTEGER PRIMARY KEY,
-    descripcion TEXT,
-    accion TEXT,
+CREATE TABLE Registro_de_actividades (
+    id_Actividad INT PRIMARY KEY,
+    descripcion VARCHAR(100),
+    accion VARCHAR(100),
     fecha_Hora DATETIME,
-    id_usuario INTEGER,
-    id_Empresa INTEGER,
-    id_Postulante INTEGER,
+    id_usuario INT,
+    id_Empresa INT,
+    id_Postulante INT,
     FOREIGN KEY (id_Postulante) REFERENCES Postulante(id_Postulante),
     FOREIGN KEY (id_Empresa) REFERENCES Empresa(id_Empresa)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Documentos (
-    id_Documento INTEGER PRIMARY KEY,
-    nom_Documento TEXT,
-    tipo_Documento TEXT,
-    size INTEGER,
+CREATE TABLE Documentos (
+    id_Documento INT PRIMARY KEY,
+    nom_Documento VARCHAR(200),
+    tipo_Documento VARCHAR(50),
+    size INT,
     fecha_Subida DATETIME,
-    ubicacion_Archivo TEXT,
-    descripcion TEXT,
+    ubicacion_Archivo VARCHAR(100),
+    descripcion VARCHAR(200),
     fecha_Expiracion DATETIME,
-    id_Usuario INTEGER,
-    id_Empresa INTEGER,
-    id_Postulante INTEGER,
+    id_Usuario INT,
+    id_Empresa INT,
+    id_Postulante INT,
     FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario),
     FOREIGN KEY (id_Postulante) REFERENCES Postulante(id_Postulante),
     FOREIGN KEY (id_Empresa) REFERENCES Empresa(id_Empresa)
@@ -123,47 +148,60 @@ CREATE TABLE IF NOT EXISTS Documentos (
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Oferta_de_trabajo (
-    id_Oferta INTEGER PRIMARY KEY,
-    descripcion TEXT,
-    requisitos TEXT,
-    ubicacion TEXT,
-    salario INTEGER,
-    id_Empresa INTEGER,
-    FOREIGN KEY (id_Empresa) REFERENCES Empresa(id_Empresa)
-)
-''')
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Administrador (
-    id_Admin INTEGER PRIMARY KEY,
-    nombre TEXT,
-    correo TEXT,
-    telefono TEXT,
-    id_Empresa INTEGER,
-    id_Usuario INTEGER,
+CREATE TABLE Notificaciones (
+    id_Notificacion INT PRIMARY KEY,
+    id_Usuario INT,
+    mensaje VARCHAR(200),
+    leido BOOLEAN,
+    fecha DATE,
     FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Postulaciones (
-    id_Postulacion INTEGER PRIMARY KEY,
-    fecha_Postulacion DATETIME,
-    estado TEXT,
-    id_Postulante INTEGER,
+CREATE TABLE Experiencia_Laboral (
+    id_Experiencia INT PRIMARY KEY,
+    empresa VARCHAR(100),
+    cargo VARCHAR(100),
+    fecha_Inicio DATE,
+    fecha_Fin DATE,
+    descripcion TEXT,
+    id_Postulante INT,
     FOREIGN KEY (id_Postulante) REFERENCES Postulante(id_Postulante)
 )
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Especialidad (
-    id_Especialidad INTEGER PRIMARY KEY,
-    nom_Especialidad TEXT,
-    id_Postulante INTEGER,
+CREATE TABLE Administrador (
+    id_Admin INT PRIMARY KEY,
+    nombre VARCHAR(20),
+    correo VARCHAR(100),
+    telefono VARCHAR(12),
+    id_Empresa INT,
+    id_Usuario INT,
+    FOREIGN KEY (id_Usuario) REFERENCES Usuarios(id_Usuario)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE Postulaciones (
+    id_Postulacion INT PRIMARY KEY,
+    fecha_Postulacion DATETIME,
+    estado VARCHAR(100),
+    id_Postulante INT,
+    FOREIGN KEY (id_Postulante) REFERENCES Postulante(id_Postulante)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE Especialidad (
+    id_Especialidad INT PRIMARY KEY,
+    nom_Especialidad VARCHAR(100),
+    id_Postulante INT,
     FOREIGN KEY (id_Postulante) REFERENCES Postulante(id_Postulante)
 )
 ''')
 
 conn.commit()
-conn.close() 
+
+conn.close()
