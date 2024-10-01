4df1438641c1ead9064da58164441f41d598e98f
eliasCARANZa <eliaspatata10@gmail.com>
2024-09-30 23:39:02 -0300

    creacion de pagina de gestion de validaciones de los postulantes y cambios en la pagina de Admin

--- Pagina_Principal_Admin/Pagina_Admin.js
+++ Pagina_Principal_Admin/Pagina_Admin.js
@@ -12,3 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var btnVerPostulantes = document.getElementById('btnVerVerificacionPostulante');
+    
+    btnVerPostulantes.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        alert('Redirigiendo a la página de gestión de postulantes.');
+        window.location.href = 'Gestion_Validacion_Postulante.html'; 
+    });
+});
+
+
