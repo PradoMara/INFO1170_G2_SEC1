23212dd6df9b64b47815635d10b10f042ff13981
eliasCARANZa <eliaspatata10@gmail.com>
2024-11-11 00:54:56 -0300

    cambio de colores y eliminacion de archivos que no ivan

--- perfil-18-años/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "mysql.inf.uct.cl";
-$username = "ecaranza"; 
-$password = "wd65dZXb1IXFKw-Lb"; 
-$dbname = "A2024_ecaranza"; 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-?>
